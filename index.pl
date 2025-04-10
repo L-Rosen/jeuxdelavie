4c1c66271a38f3a05141b3545b967cf8a52c0a9b
Lucas Rosenzweig <xeit0w@gmail.com>
2025-04-11 00:56:33 +0200

    Evolution - Règles de survie / mort

--- index.pl
+++ index.pl
@@ -30,13 +30,51 @@ voisins(Grille,Ligne,Colonne,ListeVoisine):-
     ColonneGauche is Colonne - 1,                   %On calcule l'indice de la colonne de gauche
     ColonneDroite is Colonne + 1,                   %On calcule l'indice de la colonne de droite
 
-    cellule(Grille,LigneHaut,Colonne,Haut),         %On récupère la cellule du haut
-    cellule(Grille,LigneBas,Colonne,Bas),           %On récupère la cellule du bas
-    cellule(Grille,Ligne,ColonneGauche,Gauche),     %On récupère la cellule de gauche
-    cellule(Grille,Ligne,ColonneDroite,Droite),     %On récupère la cellule de droite
+    cellule(Grille,LigneHaut,Colonne,Haut),             %Voisin du dessus
+    cellule(Grille,LigneHaut,ColonneGauche,GaucheHaut), %Voisin du haut gauche
+    cellule(Grille,LigneHaut,ColonneDroite,DroiteHaut), %Voisin du haut droite
 
-    ListeVoisine = [Haut,Droite,Bas,Gauche].        %On assemble la liste
+    cellule(Grille,LigneBas,Colonne,Bas),               %On récupère la cellule du bas
+    cellule(Grille,LigneBas,ColonneGauche,GaucheBas),   %On récupère la cellule du bas gauche
+    cellule(Grille,LigneBas,ColonneDroite,DroiteBas),   %On récupère la cellule du bas droite
+
+    cellule(Grille,Ligne,ColonneGauche,Gauche),         %On récupère la cellule de gauche
+    cellule(Grille,Ligne,ColonneDroite,Droite),         %On récupère la cellule de droite
+
+    ListeVoisine = [GaucheHaut,Haut,DroiteHaut,Gauche,Droite,GaucheBas,Bas,DroiteBas]. %On crée la liste de voisins vivants
 
 nb_voisins_vivants(Grille, Ligne, Colonne, Nombre):-
     voisins(Grille,Ligne,Colonne,ListeVoisine),     %On récupère la liste de voisins
     somme(ListeVoisine,Nombre).                     %On somme les voisins vivants
+
+
+%On vérifie si une cellule vivante survie ou meurt
+survie(Grille,Ligne,Colonne,EtatSuivant):-
+
+    cellule(Grille,Ligne,Colonne,1),                  %On récupère la cellule si elle est vivante
+    nb_voisins_vivants(Grille,Ligne,Colonne,Nombre),  %On récupère le nombre de voisins vivants
+
+    Nombre >= 2,Nombre =< 3,EtatSuivant = 1.          %On vérifie si la cellule survie                 
+%Si la cellule ne survie pas survie donnera false
+
+%On vérifie si une cellule morte nait ou reste morte
+naissance(Grille,Ligne,Colonne,EtatSuivant):-
+
+    cellule(Grille,Ligne,Colonne,0),                  %On récupère la cellule si elle est morte
+    nb_voisins_vivants(Grille,Ligne,Colonne,Nombre),  %On récupère le nombre de voisins vivants
+
+    Nombre =:= 3,EtatSuivant = 1.                     %On vérifie si la cellule nait
+naissance(_,_,_,0).                                   %Si les conditions ne sont pas remplies, la cellule reste morte
+%Si la cellule ne respecte pas les conditions a savoir être déja morte ou avoir 3 voisins vivants, EtatSuivant sera 0
+
+%On calcule chacune des règles du jeu de la vie
+etat_suivant_cellule(Grille, Ligne, Colonne,EtatSuivant):-
+    survie(Grille,Ligne,Colonne,EtatSuivant);       %On vérifie si la cellule survie
+    naissance(Grille,Ligne,Colonne,EtatSuivant).    %On vérifie si la cellule nait
+
+% Le point-virgule (`;`) permet de faire un "OU logique" entre les deux règles : 
+% - Si `survie/4` réussit (la cellule survit), alors `EtatSuivant` sera mis à 1 (cellule vivante).
+% - Si `survie/4` échoue (la cellule ne survit pas ou est déjà morte), on passe à `naissance/4` pour vérifier si la cellule peut naître.
+%   Si la cellule est morte et a exactement 3 voisins vivants, alors `EtatSuivant` sera mis à 1 (la cellule naît).
+%   Sinon si la cellule est vivante ou bien a plus ou moins de 3 voisins vivants, alors EtatSuivant sera mis à 0.
+%   Cela fonctionne car si naissance est testé cela signifie que survie a échoué, donc la cellule est morte.
